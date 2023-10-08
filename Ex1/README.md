7e86951231d41250e2e02c9ba97741e1819efd36
LucasManfrin <124399895+LucasManfrin@users.noreply.github.com>
2023-10-08 11:33:44 -0300

    Primeiro commit - Ex1

---
+++ Ex1/README.md
@@ -0,0 +1,3 @@
+Curso Web Moderno - Exercícios Javascrip
+
+01) Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração, multiplicação e divisão desses valores.
---
+++ Ex1/script.js
@@ -0,0 +1,7 @@
+function conta(a,b) {
+    console.log(`A soma de ${a} + ${b} é ${a+b}.`)
+    console.log(`A subtração de ${a} - ${b} é ${a-b}.`)
+    console.log(`A multiplicação de ${a} X ${b} é ${a*b}.`)
+    console.log(`A divisão de ${a} / ${b} é ${a/b}.`)
+}
+conta(2,4)
